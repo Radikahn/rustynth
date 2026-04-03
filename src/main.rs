fa96e2b555ec26a460146a2a65b3352731e82380
Radman <radikahn@gmail.com>
2026-04-02 23:06:47 -0700

    chore: allow dynamic calls and fq changes

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,67 @@
 use cpal::{
+    FromSample, Sample, SampleFormat, SizedSample,
     traits::{DeviceTrait, HostTrait, StreamTrait},
-    Sample, SampleFormat,
 };
 
+/// Silence Writer to Buffer
 fn write_silence<T: Sample>(data: &mut [T], _: &cpal::OutputCallbackInfo) {
     for sample in data.iter_mut() {
         *sample = Sample::EQUILIBRIUM;
     }
 }
 
+/// Write sample to audio buffer
+fn write_data<T>(output: &mut [T], channels: usize, next_sample: &mut dyn FnMut() -> f32)
+where
+    T: Sample + FromSample<f32>,
+{
+    for frame in output.chunks_mut(channels) {
+        let value: T = T::from_sample(next_sample());
+        for sample in frame.iter_mut() {
+            *sample = value;
+        }
+    }
+}
+
+pub fn run<T>(device: &cpal::Device, config: cpal::StreamConfig) -> Result<(), anyhow::Error>
+where
+    T: SizedSample + FromSample<f32>,
+{
+    let sample_rate = config.sample_rate as f32;
+    let channels = config.channels as usize;
+
+    let mut sample_clock = 0f32;
+    let mut next_value = move || {
+        sample_clock = (sample_clock + 1.0) % sample_rate;
+        (sample_clock * 440.0 * 2.0 * std::f32::consts::PI / sample_rate).sin()
+    };
+
+    let err_fn = |err| eprintln!("an error occurred on stream: {err}");
+
+    let stream = device.build_output_stream(
+        &config,
+        move |data: &mut [T], _: &cpal::OutputCallbackInfo| {
+            write_data(data, channels, &mut next_value)
+        },
+        err_fn,
+        None,
+    )?;
+    stream.play()?;
+
+    std::thread::sleep(std::time::Duration::from_millis(1000));
+
+    Ok(())
+}
+
 fn main() {
+    //support for other audio devices must be enabled at compile time
+    // #[allow(unused_mut, unused_assignments)]
+    // let mut jack_host_id = Err(HostUnavailable);
+    // #[allow(unused_mut, unused_assignments)]
+    // let mut pulseaudio_host_id = Err(HostUnavailable);
+    // #[allow(unused_mut, unused_assignments)]
+    // let mut pipewire_host_id = Err(HostUnavailable);
+
     let host = cpal::default_host();
 
     let device = host
@@ -19,28 +71,18 @@ fn main() {
     let mut supported_configs_range = device
         .supported_output_configs()
         .expect("error while querying configs");
+
     let supported_config = supported_configs_range
         .next()
         .expect("no supported config?!")
         .with_max_sample_rate();
 
-    let err_fn = |err| eprintln!("an error occurred on the output audio stream: {}", err);
     let sample_format = supported_config.sample_format();
-    let config = supported_config.into();
-    let stream = match sample_format {
-        SampleFormat::F32 => {
-            device.build_output_stream(&config, write_silence::<f32>, err_fn, None)
-        }
-        SampleFormat::I16 => {
-            device.build_output_stream(&config, write_silence::<i16>, err_fn, None)
-        }
-        SampleFormat::U16 => {
-            device.build_output_stream(&config, write_silence::<u16>, err_fn, None)
-        }
+    let config: cpal::StreamConfig = supported_config.into();
+    match sample_format {
+        SampleFormat::F32 => run::<f32>(&device, config).unwrap(),
+        SampleFormat::I16 => run::<i16>(&device, config).unwrap(),
+        SampleFormat::U16 => run::<u16>(&device, config).unwrap(),
         sample_format => panic!("Unsupported sample format '{sample_format}'"),
     }
-    .unwrap();
-
-    stream.play().unwrap();
 }
-
